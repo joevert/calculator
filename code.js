718a75c7e6f4eba2fa8cd561720cb95eed01b1af
joevert <joao_marcos99@hotmail.com>
2024-05-24 15:43:13 -0300

    link numbers buttons to display

--- code.js
+++ code.js
@@ -2,6 +2,22 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const btnNumbers = document.querySelectorAll('.num');
+
+const display = document.querySelector('.display');
+
+btnNumbers.forEach(button => {
+    button.addEventListener('click', () => {
+        if (display.textContent === '0') {
+            display.textContent = button.textContent;
+        } else {
+            display.textContent += button.textContent;
+        }
+    });
+});
+
+
+
 function operate(firstNumber, secondNumber, operator) {
     if (operator === '+')
         sum(firstNumber, secondNumber);
